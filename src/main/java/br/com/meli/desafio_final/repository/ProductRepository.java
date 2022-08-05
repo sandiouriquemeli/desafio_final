048ced4647aa70d02b744acf1b5c7ebd912ba592
BelAlbuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-08-05 13:37:24 -0300

    feat: implementa repositorios para cada entidade

--- src/main/java/br/com/meli/desafio_final/repository/ProductRepository.java
+++ src/main/java/br/com/meli/desafio_final/repository/ProductRepository.java
@@ -1,9 +1,10 @@
 package br.com.meli.desafio_final.repository;
 
-import br.com.meli.desafio_final.model.entity.Adsense;
+import br.com.meli.desafio_final.model.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface AdsenseRepo extends CrudRepository<Adsense, Long> {
+public interface ProductRepository extends CrudRepository<Product, Long> {
 }
