9f00cb09ce44bd6ef5ec3d1cd3df8085d24c5dca
andherreraML <andrea.herrera@mercadolivre.com>
2022-08-05 11:06:53 -0400

    feat: relacionamento de entidades Adsense, Batch e InBoundOrder

--- src/main/java/br/com/meli/desafio_final/model/entity/Adsense.java
+++ src/main/java/br/com/meli/desafio_final/model/entity/Adsense.java
@@ -1,10 +1,24 @@
 package br.com.meli.desafio_final.model.entity;
 
-public class Adsense {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Setter @Getter
+public class Adsense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
     Seller seller;
     Product product;
     float price;
-    
+
+    @OneToMany(mappedBy = "adsense")
+    @JsonIgnoreProperties("adsense")
+    private List<Batch> batchList;
 }
