5299685d86fcc739ea96d9e1823238bda340ebed
BelAlbuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-08-07 17:44:45 -0300

    fix: corrige tipagem de retorno dos metodos create e update

--- src/main/java/br/com/meli/desafio_final/service/IInBoundOrderService.java
+++ src/main/java/br/com/meli/desafio_final/service/IInBoundOrderService.java
@@ -1,5 +1,6 @@
 package br.com.meli.desafio_final.service;
 
+import br.com.meli.desafio_final.dto.InBoundOrderDto;
 import br.com.meli.desafio_final.model.entity.InBoundOrder;
 
 import java.util.List;
@@ -7,6 +8,6 @@ import java.util.List;
 public interface IInBoundOrderService {
 
     List<InBoundOrder> getAll();
-    InBoundOrder create(InBoundOrder inBoundOrder);
-    InBoundOrder update(InBoundOrder inBoundOrder);
+    List<InBoundOrderDto> create(InBoundOrder inBoundOrder);
+    List<InBoundOrderDto> update(InBoundOrder inBoundOrder);
 }
