8f326506e324fb5c071f87ea0a8df1fd858c79d3
BelAlbuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-08-07 17:48:11 -0300

    feat: implementa os metodos create e update na camada de serviço

--- src/main/java/br/com/meli/desafio_final/service/InBoundOrderService.java
+++ src/main/java/br/com/meli/desafio_final/service/InBoundOrderService.java
@@ -1,28 +1,84 @@
 package br.com.meli.desafio_final.service;
 
-import br.com.meli.desafio_final.model.entity.InBoundOrder;
-import br.com.meli.desafio_final.repository.InboundOrderRepository;
+import br.com.meli.desafio_final.dto.InBoundOrderDto;
+import br.com.meli.desafio_final.model.entity.*;
+import br.com.meli.desafio_final.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class InBoundOrderService implements IInBoundOrderService {
+
     @Autowired
     InboundOrderRepository repository;
+
+    @Autowired
+    ValidationService validationService;
+
+    @Autowired
+    AdsenseService adsenseService;
+
+    @Autowired
+    BatchService batchService;
 
     @Override
     public List<InBoundOrder> getAll() {
         return repository.findAll();
     }
 
-    @Override
-    public InBoundOrder create(InBoundOrder inBoundOrder) {
-        return repository.save(inBoundOrder);
+    //TODO: Fazer exceptions para Seller, Section e Product notFound
+    // TODO: lembrar de criar um service pra cada ou um service validations
+
+    /**
+     * Metodo valida o InboundOrder e retorna a lista de Baths já validada.
+     * @param inBoundOrder
+     * @return BatchList
+     */
+    private List<Batch> validateInboundOrder(InBoundOrder inBoundOrder) {
+        validationService.validateSection(inBoundOrder.getSection());
+        List<Batch>batchList = inBoundOrder.getBatchStock();
+        batchList.forEach((batch) -> {
+            Adsense adsense = adsenseService.findById(batch.getAdsense().getId());
+            validationService.validateSeller(adsense.getSeller());
+            validationService.validateProduct(adsense.getProduct());
+        });
+        return batchList;
+    }
+
+    private List<InBoundOrderDto> saveOrUpdate(InBoundOrder inBoundOrder) {
+
+        List<Batch> batchList = this.validateInboundOrder(inBoundOrder);
+
+        InBoundOrder newInboundOrder = repository.save(inBoundOrder);
+
+        return batchList.stream().map((batch -> {
+            batch.setInBoundOrder(newInboundOrder);
+            return new InBoundOrderDto(batchService.saveBatch(batch));
+        })).collect(Collectors.toList());
     }
 
     @Override
-    public InBoundOrder update(InBoundOrder inBoundOrder) {
-        return null;
+    public List<InBoundOrderDto> create(InBoundOrder inBoundOrder) {
+       if(inBoundOrder.getId() != null) {
+               throw new RuntimeException("InboundOrder já cadastrada");
+       }
+
+      return saveOrUpdate(inBoundOrder);
     }
+
+    @Override
+    public List<InBoundOrderDto> update(InBoundOrder inBoundOrder) {
+        repository.findById(inBoundOrder.getId()).orElseThrow(() -> {
+            throw new RuntimeException("InboundOrder não encontrada");
+        });
+        return saveOrUpdate(inBoundOrder);
+    }
+
+    // TODO: fazer exception pra update e create
+    // TODO: Registrar o representante junto com o inboundOrder
+    // TODO: validar que o setor corresponde ao tipo de produto
+    // TODO: validar que o setor tem espaço disponível
 }
