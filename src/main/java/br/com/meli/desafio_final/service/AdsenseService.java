c85cfcdc3d76eab139929e674826e3f8b11b173f
BelAlbuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-08-07 17:41:48 -0300

    feat: implementa metodo findAdsenseById na camada service

---
+++ src/main/java/br/com/meli/desafio_final/service/AdsenseService.java
@@ -0,0 +1,19 @@
+package br.com.meli.desafio_final.service;
+
+import br.com.meli.desafio_final.model.entity.Adsense;
+import br.com.meli.desafio_final.repository.AdsenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdsenseService implements IAdsenseService{
+    @Autowired
+    AdsenseRepository adsenseRepository;
+
+    @Override
+    public Adsense findById(long id) {
+        return adsenseRepository.findById(id)
+                .orElseThrow(() -> { throw new RuntimeException("deu ruim"); } );
+    }
+}
+    // TODO: Tratar Exception
---
+++ src/main/java/br/com/meli/desafio_final/service/IAdsenseService.java
@@ -0,0 +1,7 @@
+package br.com.meli.desafio_final.service;
+
+import br.com.meli.desafio_final.model.entity.Adsense;
+
+public interface IAdsenseService {
+    Adsense findById(long id);
+}
