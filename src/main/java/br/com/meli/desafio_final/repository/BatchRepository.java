070215ef093f085dacc09257a2a317ab652e00fb
BelAlbuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-08-07 16:26:38 -0300

    fix: muda a interface dos repositorios de CrudRepository para JpaRepository

--- src/main/java/br/com/meli/desafio_final/repository/BatchRepository.java
+++ src/main/java/br/com/meli/desafio_final/repository/BatchRepository.java
@@ -1,9 +1,9 @@
 package br.com.meli.desafio_final.repository;
 
 import br.com.meli.desafio_final.model.entity.Batch;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BatchRepository extends CrudRepository<Batch, Long> {
+public interface BatchRepository extends JpaRepository<Batch, Long> {
 }
