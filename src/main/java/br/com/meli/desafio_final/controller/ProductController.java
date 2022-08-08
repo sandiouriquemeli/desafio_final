668d7a8f34d08b90d3edab01cb148d89e62ac512
Letícia Castro <leticia.castro@mercadolivre.com>
2022-08-08 11:42:50 -0300

    Alterada a versão da rota de produtos

--- src/main/java/br/com/meli/desafio_final/controller/ProductController.java
+++ src/main/java/br/com/meli/desafio_final/controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/fresh-products")
+@RequestMapping("/api/v2/fresh-products") //adicionando endpoint v2 -> requisito 2
 public class ProductController {
 
     @Autowired
