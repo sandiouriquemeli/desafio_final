3c91a249e29cee22259036693b6e14d93d7fea2e
Letícia Castro <leticia.castro@mercadolivre.com>
2022-08-08 16:01:16 -0300

    Implementado teste que verifica se dispara a exceção quando não há anúncios cadastrados

--- src/test/java/br/com/meli/desafio_final/service/AdsenseServiceTest.java
+++ src/test/java/br/com/meli/desafio_final/service/AdsenseServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -48,15 +49,15 @@ class AdsenseServiceTest {
 
     @Test
     @DisplayName("Busca pelo ID: Valida se dispara a exceção NOT FOUND quando o ID é inválido.")
-    void findById_throwException_whenIdIdInvalid() {
+    void findById_throwException_whenIdInvalid() {
         assertThrows(ExNotFound.class, () -> {
            service.findById(0L);
         });
     }
 
     @Test
-    @DisplayName("Listar anúncios: Valida se retorna uma lista de anúncios")
-    void findAll() {
+    @DisplayName("Listar anúncios: Valida se retorna uma lista de anúncios.")
+    void findAll_returnListAdsense_whenAdsensesExists() {
         BDDMockito.when(repository.findAll())
             .thenReturn(List.of(TestAdsenseGenerator.getAdsenseWithId()));
 
@@ -64,5 +65,14 @@ class AdsenseServiceTest {
 
         assertThat(adsenseList).isNotNull();
         assertThat(adsenseList.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Listar anúncios: Valida se dispara a execeção NOT FOUND quando não há anúncios cadastrados.")
+    void findAll_throwException_whenAdsensesNotExists() {
+
+        assertThrows(ExNotFound.class, () -> {
+            service.findAll();
+        });
     }
 }
