d0d099ccb7dae80d4fa8ffe36d1d6cf56a852ef2
andherreraML <andrea.herrera@mercadolivre.com>
2022-08-08 14:14:29 -0400

    feat: adiciona metodos utils para testes de Adsense

--- src/main/java/br/com/meli/desafio_final/model/entity/Adsense.java
+++ src/main/java/br/com/meli/desafio_final/model/entity/Adsense.java
@@ -1,14 +1,16 @@
 package br.com.meli.desafio_final.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Setter @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Adsense {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
