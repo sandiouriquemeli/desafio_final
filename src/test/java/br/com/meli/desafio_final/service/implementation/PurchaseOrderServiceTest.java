ff858d9e0d26cdee142a71fc5e4c05dfa81e018a
Letícia Castro <leticia.castro@mercadolivre.com>
2022-08-09 15:16:25 -0300

    Implementado teste que busca pelo ID em PurchaseOrderServiceTest

--- src/test/java/br/com/meli/desafio_final/service/implementation/PurchaseOrderServiceTest.java
+++ src/test/java/br/com/meli/desafio_final/service/implementation/PurchaseOrderServiceTest.java
@@ -1,14 +1,22 @@
 package br.com.meli.desafio_final.service.implementation;
 
+import br.com.meli.desafio_final.model.entity.PurchaseOrder;
 import br.com.meli.desafio_final.repository.PurchaseOrderRepository;
+import br.com.meli.desafio_final.util.PurchaseOrderUtils;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -17,9 +25,20 @@ public class PurchaseOrderServiceTest {
     @InjectMocks
     private PurchaseOrderService service;
 
+    @Mock
     private PurchaseOrderRepository repository;
 
     @Test
-    void findById() {
+    @DisplayName("Busca pelo ID: Valida se retorna uma Ordem de Compra quando um ID é válido")
+    void findById_returnPurchaseOrder_whenIdValid() {
+        BDDMockito.when(repository.findById(anyLong()))
+            .thenReturn(Optional.of(PurchaseOrderUtils.newPurchase1ToSave()));
+
+        PurchaseOrder purchaseOrder = PurchaseOrderUtils.newPurchase1ToSave();
+
+        PurchaseOrder purchaseFound = service.findById(1L);
+
+        assertThat(purchaseFound).isNotNull();
+        assertThat(purchaseFound.getId()).isEqualTo(purchaseOrder.getId());
     }
 }
