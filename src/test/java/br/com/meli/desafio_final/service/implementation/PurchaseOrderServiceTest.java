2ab42e98e66bee99719c2b1c6e88e39ac99660b9
Letícia Castro <leticia.castro@mercadolivre.com>
2022-08-09 17:48:15 -0300

    Implementado o teste que valida a busca de anúncios pelo id da ordem de compra

--- src/test/java/br/com/meli/desafio_final/service/implementation/PurchaseOrderServiceTest.java
+++ src/test/java/br/com/meli/desafio_final/service/implementation/PurchaseOrderServiceTest.java
@@ -1,9 +1,14 @@
 package br.com.meli.desafio_final.service.implementation;
 
+import br.com.meli.desafio_final.dto.AdsenseDto;
 import br.com.meli.desafio_final.exception.NotFound;
+import br.com.meli.desafio_final.model.entity.Adsense;
+import br.com.meli.desafio_final.model.entity.Item;
 import br.com.meli.desafio_final.model.entity.PurchaseOrder;
 import br.com.meli.desafio_final.model.enums.Status;
 import br.com.meli.desafio_final.repository.PurchaseOrderRepository;
+import br.com.meli.desafio_final.util.AdsenseUtils;
+import br.com.meli.desafio_final.util.ItemUtils;
 import br.com.meli.desafio_final.util.PurchaseOrderUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +21,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
+import javax.persistence.MapKeyClass;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.atLeastOnce;
@@ -32,6 +40,9 @@ public class PurchaseOrderServiceTest {
 
     @Mock
     private PurchaseOrderRepository repository;
+
+    @Mock
+    private ItemService itemService;
 
     @Test
     @DisplayName("Busca pelo ID: Valida se retorna uma Ordem de Compra quando um ID é válido.")
@@ -57,7 +68,7 @@ public class PurchaseOrderServiceTest {
 
     @Test
     @DisplayName("Atualiza Status: Valida se retorna uma Ordem de Compra com status atualizado e se ela foi salva.")
-    void updateToFinished() {
+    void updateToFinished_returnPurchaseOrderUpdated_whenUpdateStatus() {
         PurchaseOrder purchaseOrder = PurchaseOrderUtils.newPurchase1ToSave();
 
         assertThat(purchaseOrder.getStatus()).isEqualTo(Status.OPEN);
@@ -70,7 +81,21 @@ public class PurchaseOrderServiceTest {
         verify(repository, atLeastOnce()).save(purchaseOrder);
     }
 
+    @Test
+    @DisplayName("Busca Anúncios pelo ID da Ordem de Compra: Valida se retorna uma Lista de Anúncios " +
+        "quando o ID da Ordem de Compra é válido.")
     void findAdsensesByPurchaseOrderId() {
+        BDDMockito.when(itemService.findItemsByPurchaseOrderId(anyLong()))
+            .thenReturn(ItemUtils.generatedItemList());
 
+        List<Item> itemList = itemService.findItemsByPurchaseOrderId(1L);
+
+        List<Adsense> adsenseList = new ArrayList<>();
+        itemList.forEach(item -> adsenseList.add(item.getAdsense()));
+
+        List<AdsenseDto> adsenseDtoList = AdsenseDto.convertDto(adsenseList);
+
+        assertThat(adsenseDtoList).isNotNull();
+        assertThat(adsenseDtoList.size()).isEqualTo(ItemUtils.generatedItemList().size());
     }
 }
