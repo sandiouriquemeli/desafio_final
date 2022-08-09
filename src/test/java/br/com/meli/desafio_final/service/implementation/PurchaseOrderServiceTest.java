b90084c40caf89ca48100cfe4e4cf0fba16f357f
Letícia Castro <leticia.castro@mercadolivre.com>
2022-08-09 16:09:42 -0300

    Implementado teste que validade o update em PurchaseOrderServiceTest

--- src/test/java/br/com/meli/desafio_final/service/implementation/PurchaseOrderServiceTest.java
+++ src/test/java/br/com/meli/desafio_final/service/implementation/PurchaseOrderServiceTest.java
@@ -2,6 +2,7 @@ package br.com.meli.desafio_final.service.implementation;
 
 import br.com.meli.desafio_final.exception.NotFound;
 import br.com.meli.desafio_final.model.entity.PurchaseOrder;
+import br.com.meli.desafio_final.model.enums.Status;
 import br.com.meli.desafio_final.repository.PurchaseOrderRepository;
 import br.com.meli.desafio_final.util.PurchaseOrderUtils;
 import org.junit.jupiter.api.Assertions;
@@ -17,6 +18,8 @@ import org.mockito.quality.Strictness;
 
 import java.util.Optional;
 
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 
@@ -31,7 +34,7 @@ public class PurchaseOrderServiceTest {
     private PurchaseOrderRepository repository;
 
     @Test
-    @DisplayName("Busca pelo ID: Valida se retorna uma Ordem de Compra quando um ID é válido")
+    @DisplayName("Busca pelo ID: Valida se retorna uma Ordem de Compra quando um ID é válido.")
     void findById_returnPurchaseOrder_whenIdValid() {
         BDDMockito.when(repository.findById(anyLong()))
             .thenReturn(Optional.of(PurchaseOrderUtils.newPurchase1ToSave()));
@@ -45,10 +48,29 @@ public class PurchaseOrderServiceTest {
     }
 
     @Test
-    @DisplayName("Busca pelo ID: Valida se retorna uma exceção quando o ID é inválido")
+    @DisplayName("Busca pelo ID: Valida se retorna uma exceção quando o ID é inválido.")
     void findById_throwException_whenIdInvalid() {
         Assertions.assertThrows(NotFound.class, () -> {
             service.findById(0L);
         });
+    }
+
+    @Test
+    @DisplayName("Atualiza Status: Valida se retorna uma Ordem de Compra com status atualizado e se ela foi salva.")
+    void updateToFinished() {
+        PurchaseOrder purchaseOrder = PurchaseOrderUtils.newPurchase1ToSave();
+
+        assertThat(purchaseOrder.getStatus()).isEqualTo(Status.OPEN);
+
+        purchaseOrder.setStatus(Status.FINISHED);
+        repository.save(purchaseOrder);
+
+        assertThat(purchaseOrder).isNotNull();
+        assertThat(purchaseOrder.getStatus()).isEqualTo(Status.FINISHED);
+        verify(repository, atLeastOnce()).save(purchaseOrder);
+    }
+
+    void findAdsensesByPurchaseOrderId() {
+
     }
 }
