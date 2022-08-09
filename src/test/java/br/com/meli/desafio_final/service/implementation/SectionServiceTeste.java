0c10b675fde1ffc7bfb9bc81816e2ec47812c487
BelAlbuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-08-09 19:30:24 -0300

    feat: conclui testes da camada service e refatora

--- src/test/java/br/com/meli/desafio_final/service/implementation/SectionServiceTeste.java
+++ src/test/java/br/com/meli/desafio_final/service/implementation/SectionServiceTeste.java
@@ -1,4 +1,4 @@
-package br.com.meli.desafio_final.service;
+package br.com.meli.desafio_final.service.implementation;
 
 import br.com.meli.desafio_final.model.entity.Section;
 import br.com.meli.desafio_final.repository.SectionRepository;
