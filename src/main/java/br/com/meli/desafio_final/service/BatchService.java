57b31cb1e34b8aedc153c79fb1a269e81e6aac88
andherreraML <andrea.herrera@mercadolivre.com>
2022-08-10 11:48:26 -0400

    refactor: metodo de retorno do BatchStock e implementa um metodo de sort

--- src/main/java/br/com/meli/desafio_final/service/BatchService.java
+++ src/main/java/br/com/meli/desafio_final/service/BatchService.java
@@ -1,5 +1,6 @@
 package br.com.meli.desafio_final.service;
 
+import br.com.meli.desafio_final.dto.AdsenseIdDto;
 import br.com.meli.desafio_final.dto.BatchDto;
 import br.com.meli.desafio_final.exception.NotFound;
 import br.com.meli.desafio_final.model.entity.Batch;
@@ -7,7 +8,10 @@ import br.com.meli.desafio_final.repository.BatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BatchService implements IBatchService{
@@ -28,5 +32,27 @@ public class BatchService implements IBatchService{
     @Override
     public List<BatchDto> findAllByAdsenseId(Long adsenseId) {
         return BatchDto.convertDto(batchRepository.findAllByAdsenseId(adsenseId));
+    }
+
+    @Override
+    public List<BatchDto> returnBatchStock(List<AdsenseIdDto> adsenseList, String s) {
+        List<BatchDto> result = new ArrayList<>();
+        List<List<BatchDto>> batchDtoList = adsenseList.stream().map(adsenseId -> findAllByAdsenseId(adsenseId.getId()))
+                .collect(Collectors.toList());
+        batchDtoList.forEach(result::addAll);
+        if (s != null) sortBatchList(result, s);
+        return result;
+    }
+
+    private void sortBatchList(List<BatchDto> batchDtos, String s) {
+        if (s.equals("L")) {
+            batchDtos.sort(Comparator.comparing(BatchDto::getBatchNumber));
+        }
+        if (s.equals("Q")) {
+            batchDtos.sort(Comparator.comparing(BatchDto::getCurrentQuantity));
+        }
+        if (s.equals("V")) {
+            batchDtos.sort(Comparator.comparing(BatchDto::getDueDate));
+        }
     }
 }
