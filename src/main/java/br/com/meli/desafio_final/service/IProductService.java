df7466aceee43f602ff1415741ee9c5301b26362
andherreraML <andrea.herrera@mercadolivre.com>
2022-08-10 08:47:51 -0400

    feat: implementa os metodos findBatchByProduct e findById em Product

--- src/main/java/br/com/meli/desafio_final/service/IProductService.java
+++ src/main/java/br/com/meli/desafio_final/service/IProductService.java
@@ -1,5 +1,7 @@
 package br.com.meli.desafio_final.service;
 
+import br.com.meli.desafio_final.dto.BatchDto;
+import br.com.meli.desafio_final.dto.BatchesByProductDto;
 import br.com.meli.desafio_final.model.entity.Product;
 import br.com.meli.desafio_final.model.enums.Category;
 
@@ -7,7 +9,8 @@ import java.util.List;
 
 public interface IProductService {
 
-    List<Product> getAllProducts();
-    List<Product> getByCategory(Category category);
-
+    List<Product> findAllProducts();
+    List<Product> findByCategory(Category category);
+    Product findById(Long id);
+    BatchesByProductDto findBatchByProduct(Long id);
 }
--- src/main/java/br/com/meli/desafio_final/service/ProductService.java
+++ src/main/java/br/com/meli/desafio_final/service/ProductService.java
@@ -1,13 +1,19 @@
 package br.com.meli.desafio_final.service;
 
+import br.com.meli.desafio_final.dto.AdsenseIdDto;
+import br.com.meli.desafio_final.dto.BatchDto;
+import br.com.meli.desafio_final.dto.BatchesByProductDto;
 import br.com.meli.desafio_final.exception.NotFound;
 import br.com.meli.desafio_final.model.entity.Product;
+import br.com.meli.desafio_final.model.entity.Section;
 import br.com.meli.desafio_final.model.enums.Category;
 import br.com.meli.desafio_final.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService implements IProductService {
@@ -15,8 +21,17 @@ public class ProductService implements IProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Autowired
+    private AdsenseService adsenseService;
+
+    @Autowired
+    private BatchService batchService;
+
+    @Autowired
+    private SectionService sectionService;
+
     @Override
-    public List<Product> getAllProducts() {
+    public List<Product> findAllProducts() {
         List<Product> products = repository.findAll();
 
         if (products.size() == 0) throw new NotFound("Lista de produtos não encontrada");
@@ -25,11 +40,31 @@ public class ProductService implements IProductService {
     }
 
     @Override
-    public List<Product> getByCategory(Category category) {
+    public List<Product> findByCategory(Category category) {
         List<Product> response = repository.findByCategory(category);
         if (response.size() == 0) {
             throw new RuntimeException("Nenhum produto com essa categoria foi encontrado");
         }
         return response;
     }
+
+    @Override
+    public Product findById(Long id) {
+        return repository.findById(id).orElseThrow(() -> {
+            throw new NotFound("Produto inexistente");
+        });
+    }
+
+    public BatchesByProductDto findBatchByProduct(Long id) {
+        List<BatchDto> result = new ArrayList<>();
+        Product product = findById(id);
+        Section section = sectionService.findByCategory(product.getCategory());
+        List<AdsenseIdDto> adsenseList = adsenseService.findByProductId(product.getId());
+        List<List<BatchDto>> batchDtoList = adsenseList.stream().map(adsenseId -> batchService.findAllByAdsenseId(adsenseId.getId()))
+                .collect(Collectors.toList());
+        batchDtoList.stream().forEach(batchDtos -> result.addAll(batchDtos));
+        return new BatchesByProductDto(section, product.getId(), result);
+    }
+
+
 }
